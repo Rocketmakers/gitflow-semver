ebe63e8bc6092dc647338b05305a4fef116b7822
Jonny Stoten <jonny@rocketmakers.com>
2015-04-10 15:13:57 +0100

    Add OS X install script for git flow

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+git flow version > /dev/null
+if [ $? != 0 ]; then
+  echo "Installing git flow..."
+  brew install git-flow
+  echo "Done!"
+fi
+
 echo "Installing..."
 cp git-release /usr/local/bin
 echo "Done!"
