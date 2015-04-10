5f9404052952b81f5a1e4a2e530813508e117eac
Jonny Stoten <jonny@rocketmakers.com>
2015-04-10 15:14:14 +0100

    Add windows install script for gitflow

--- install.bat
+++ install.bat
@@ -1,1 +1,10 @@
+git flow version > NUL
+if %ERRORLEVEL% GEQ 1 (
+  copy gitflow-windows-dependencies\* "C:\Program Files (x86)\Git\bin"
+  cd gitflow
+  contrib\msysgit-install.cmd "C:\Program Files (x86)\Git"
+)
+
+echo "Installing..."
 copy git-release "C:\Program Files (x86)\Git\bin"
+echo "Done!"
